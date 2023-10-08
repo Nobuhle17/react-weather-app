d3a173e0ac3718c8aa6e4c740741e5b6fc078e1f
Nobuhle` <shekimonobuhle@gmail.com>
2023-10-08 16:42:01 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="container">
+    <footer>
+      This Project was coded by Nobuhle and is open sourced and hosted on netlify
+    </footer>
+    </div>
     </div>
   );
 }
